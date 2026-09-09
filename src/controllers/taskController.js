eeae6aeb1db0cdf6ce0255b57a532ed926190cdb
aadharsh-09 <aadharsh.9109@gmail.com>
2026-09-09 10:58:04 +0530

    basic getTask done

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -10,4 +10,14 @@ const createTask = async (req, res) => {
     }
 };
 
-module.exports = { createTask };
+const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { createTask, getTasks };
--- src/routes/taskroutes.js
+++ src/routes/taskroutes.js
@@ -1,13 +1,9 @@
 const express = require("express");
+const { createTask, getTasks } = require("../controllers/taskController");
 
-// Import the createTask function from the controller
-const { createTask } = require("../controllers/taskController");
-
-// Create a router to handle task-related routes
 const router = express.Router();
 
-// When a POST request comes to this route, run createTask
 router.post("/", createTask);
+router.get("/", getTasks);
 
-// Export the router so app.js can use it
 module.exports = router;
