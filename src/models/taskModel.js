44abd38196b6de9344070592a0b36ffe9b522b00
aadharsh-09 <aadharsh.9109@gmail.com>
2026-09-10 08:05:37 +0530

    validation messages updated

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -3,22 +3,28 @@ const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
-        trim: true
+        required: [true, "Title is Required"],
+        trim: true,
+        minlength: [3, "Title must be at least 3 characters long"],
+        maxlength: [100, "Title cannot exceed 100 characters"]
     },
     description: {
         type: String,
         default: "",
-        trim: true
+        trim: true,
+        maxlnegth: [500, "Description cannot exceed 500 characters"]
     },
     status: {
         type: String,
-        enum: ["pending", "in-progress", "completed"],
+        enum: {
+            values: ["pending", "in-progress", "completed"],
+            message: "Status must be pending, in-progress or completed"
+        },
         default: "pending"
     },
     dueDate: {
         type: Date,
-        required: true
+        required: [true, "Due date is required"]
     }
 }, {
     timestamps: true
